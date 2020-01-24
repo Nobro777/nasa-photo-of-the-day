c71972aac7489f6629d878c05aae2301685e103f
Nobro777 <52586100+Nobro777@users.noreply.github.com>
2020-01-23 16:50:50 -0800

    mvp done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
-// import "./components/body";
 import Body from "./components/body";
-// import "./components/";
+import axios from "axios";
+
 
 function App() {
 
+  const [nasaInfo, setNasaStats] = useState([""]);
   
 
-
-
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=bNEndKHSiIA8faNAtZ98oeEtkLQHGt72udc1KJbA"
+      )
+      .then(resp => {
+        // return null;
+        console.log("nasa data -- ", resp.data.hdurl);
+        setNasaStats(resp.data);
+      })
+      .catch(error => {
+        // return null;
+        console.log("nasa data error -- ", error);
+      });
+  }, []);
 
 
   return (
     <div className="App">
-      <Body />
+    {/* styles={{ backgroundImage:`url(${props.data.hdurl})`}} */}
+      <Body data={nasaInfo} />
     </div>
   );
 }
--- src/components/body.js
+++ src/components/body.js
@@ -1,33 +1,121 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Collapse,
+    Navbar,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText } from "reactstrap";
 
 const Body = props => {
 
-const [image, setImage] = useState(props.data.hdurl);
+    const [isOpen, setIsOpen] = useState(false);
 
-useEffect(() => {
-    axios
-        .get(
-            "https://api.nasa.gov/planetary/apod?api_key=bNEndKHSiIA8faNAtZ98oeEtkLQHGt72udc1KJbA"
-        )
-        .then(resp => {
-            // return null;
-            console.log("nasa data -- ", resp)
-            // setImage(resp.data.hdurl)
-        })
-        .catch(error => {
-            // return null;
-            console.log("nasa data error -- ", error)
-        });
-    }, []);
+    const toggle = () => setIsOpen(!isOpen);
 
-
+const BodyCard = styled.div`
+color: black;
+background-color: lightgray;
+padding: 5%;
+border: 2px solid black;
+`
+const CardTitle = styled.h1`
+margin: 5% 5% 5% 5%;
+font-size: 2.3rem;
+font-family: 'ZCOOL QingKe HuangYou', cursive;
+`
+const SubTitle = styled.h3`
+margin: 5%;
+color: white;
+font-family: 'Pacifico', cursive;
+`
+const CardDate = styled.p`
+font-size: 1.5rem;
+font-weight: bold;
+font-family: 'ZCOOL QingKe HuangYou', cursive;
+`
+const CardPara = styled.p`
+margin: 5% 5% 5% 5%;
+padding: 5% 0 5% 0;
+border-top: 1px solid black;
+border-bottom: 1px solid black;
+font-family: 'Alata', sans-serif;
+`
+const ImageSize = styled.img`
+width: 85%;
+height: auto;
+`
+const Button = styled.button`
+color: black;
+margin: 5% 5% 5% 5%;
+border-radius: .5rem;
+font-weight: bold;
+&:hover {
+    background-color: orange;
+}
+`
+const NavbarBrand = styled.span`
+opacity: .2;
+font-size: 1.25rem;
+margin: 0 0 0 5%;
+display: flex;
+justify-content: space-between;
+`
 
     return (
-    <h1>
-        
-    </h1>
-    )
-}
+        <div>
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">Nasa: Picture of the Day</NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                        <NavLink href="/components/">Nav Item 1</NavLink>
+                        </NavItem>
+                        <NavItem>
+                        <NavLink href="https://github.com/LambdaSchool/nasa-photo-of-the-day" target="_blank">GitHub</NavLink>
+                        </NavItem>
+                        <NavItem>
+                        <NavLink href="/components/">Nav Item 3</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            Nav Item 4
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                            <DropdownItem>
+                            Sub Item 1
+                            </DropdownItem>
+                            <DropdownItem>
+                            Sub Item 2
+                            </DropdownItem>
+                            <DropdownItem>
+                            Reset
+                            </DropdownItem>
+                        </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                    
+                    </Collapse>
+                </Navbar>
+            </div>
+        <BodyCard className="container">
+            <CardTitle>{props.data.title}</CardTitle>
+            <ImageSize src={props.data.hdurl} alt="pic of the day"/>
+            <SubTitle>{props.data.copyright}</SubTitle>
+            <CardPara>{props.data.explanation}</CardPara>
+            <CardDate>-{props.data.date}-</CardDate>
+            <Button outline color="danger">I Like It</Button>
+            <Button outline color="danger">I Don't Like It</Button>
+        </BodyCard>
+</div>
+);
+};
 
 export default Body;
